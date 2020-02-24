56692b6cef348d5a7f3b9b1845e96aa9f89ecaa0
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-23 19:19:02 -0500

    added bookings

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,157 @@
+const bcrypt = require("bcryptjs");
+
+const Event = require("../../models/event");
+const User = require("../../models/user");
+const Booking = require("../../models/booking");
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    events.map(event => {
+      return {
+        ...event._doc,
+        _id: event.id,
+        date: new Date(event._doc.date).toISOString(),
+        creator: user.bind(this, event.creator)
+      };
+    });
+    return events;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const singleEvent = async eventId => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      creator: user.bind(this, event.creator)
+    };
+  } catch (err) {
+    throw err;
+  }
+};
+
+const user = userId => {
+  return User.findById(userId)
+    .then(user => {
+      return {
+        ...user._doc,
+        _id: user.id,
+        createdEvents: events.bind(this, user._doc.createdEvents)
+      };
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+module.exports = {
+  events: () => {
+    return Event.find()
+      .then(events => {
+        return events.map(event => {
+          return {
+            ...event._doc,
+            _id: event.id,
+            date: new Date(event._doc.date).toISOString(),
+            creator: user.bind(this, event._doc.creator)
+          };
+        });
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find();
+      return bookings.map(booking => {
+        return {
+          ...booking._doc,
+          _id: booking.id,
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event),
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString()
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  createEvent: args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: "5e52f8ca7ecd0e4088144fd6"
+    });
+    let createdEvent;
+    return event
+      .save()
+      .then(result => {
+        createdEvent = {
+          ...result._doc,
+          _id: result._doc._id.toString(),
+          date: new Date(event._doc.date).toISOString(),
+          creator: user.bind(this, result._doc.creator)
+        };
+        return User.findById("5e52f8ca7ecd0e4088144fd6");
+      })
+      .then(user => {
+        if (!user) {
+          throw new Error("User not found.");
+        }
+        user.createdEvents.push(event);
+        return user.save();
+      })
+      .then(() => {
+        return createdEvent;
+      })
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
+  },
+  createUser: args => {
+    return User.findOne({ email: args.userInput.email })
+      .then(user => {
+        if (user) {
+          throw new Error("User already exists.");
+        }
+        return bcrypt.hash(args.userInput.password, 12);
+      })
+      .then(hashedPassword => {
+        const user = new User({
+          email: args.userInput.email,
+          password: hashedPassword
+        });
+        return user.save();
+      })
+      .then(result => {
+        return { ...result._doc, password: null, _id: result._id };
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  bookEvent: async args => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      userId: "5e52f8ca7ecd0e4088144fd6",
+      event: fetchedEvent
+    });
+    const result = await booking.save();
+    return {
+      ...result._doc,
+      _id: result.id,
+      createdAt: new Date(result._doc.createdAt).toISOString(),
+      updatedAt: new Date(result._doc.updatedAt).toISOString()
+    };
+  }
+};
